89797d9b1ad4d29cc53fe0de884441b670572936
wxl <2027285515@qq.com>
2020-01-10 16:11:51 +0800

    first commit

---
+++ spk_css.cpp
@@ -0,0 +1,22 @@
+/*
+ * Sparkle Chunk Storage Subject Interface
+ *
+ * Copyright (C) 2019 CQU STARLab. All rights reserved.
+ * Author: 
+ */
+
+#include <spk_css.h>
+
+void fn()
+{
+
+}
+void time_init(void)
+{
+
+}
+
+void fn2(msg)
+{
+
+}
